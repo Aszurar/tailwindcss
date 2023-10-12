3e30f9ae2b592ea75396cf074612d411d22b3d9f
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-10-12 12:34:09 -0300

    :bento: dição dos ícones de search e logo :bento:

---
+++ src/app/assets/SearchIcon.tsx
@@ -0,0 +1,17 @@
+export function SearchIcon() {
+  return (
+    <svg
+      aria-label="Pesquisar"
+      className="fill-zinc-500 h-5 w-5"
+      xmlns="http://www.w3.org/2000/svg"
+      width="56"
+      height="56"
+      fill="#3d3d3d"
+      viewBox="0 0 256 256"
+      role="img"
+    >
+      <title>Pesquisar</title>
+      <path d="M232.49 215.51L185 168a92.12 92.12 0 10-17 17l47.53 47.54a12 12 0 0017-17zM44 112a68 68 0 1168 68 68.07 68.07 0 01-68-68z"></path>
+    </svg>
+  )
+}
