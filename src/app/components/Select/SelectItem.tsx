bba31dae64b2bddea990feac3c90033e2481b7ca
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-11-29 15:28:49 -0300

    ♻️💄 Update SelectItem and Select styles ♻️💄

--- src/app/components/Select/SelectItem.tsx
+++ src/app/components/Select/SelectItem.tsx
@@ -9,14 +9,16 @@ type SelectItemProps = Select.SelectItemProps & {
 export function SelectItem({ text, ...rest }: SelectItemProps) {
   return (
     <Select.Item
-      className="flex items-center justify-between px-3.5 py-2.5 data-[highlighted]:bg-violet-50"
+      className="flex items-center justify-between px-3.5 py-2.5 data-[highlighted]:bg-violet-50 dark:data-[highlighted]:bg-zinc-700"
       {...rest}
     >
-      <Select.ItemText className="font-medium text-black">
-        {text}
+      <Select.ItemText asChild>
+        <span className="font-medium text-black dark:text-zinc-100">
+          {text}
+        </span>
       </Select.ItemText>
       <Select.ItemIndicator>
-        <Check className="h-4 w-4 text-violet-500" />
+        <Check className="h-4 w-4 text-violet-500 dark:text-zinc-300" />
       </Select.ItemIndicator>
     </Select.Item>
   )
--- src/app/components/Select/index.tsx
+++ src/app/components/Select/index.tsx
@@ -11,13 +11,18 @@ type SelectProps = SelectPrimitive.SelectProps & {
 export function Select({ children, placeholder, ...rest }: SelectProps) {
   return (
     <SelectPrimitive.Root {...rest}>
-      <SelectPrimitive.Trigger className="flex h-11 w-full items-center justify-between rounded-lg border border-zinc-300 px-3.5 py-2.5 shadow-sm data-[placeholder]:text-zinc-500">
+      <SelectPrimitive.Trigger
+        className={`flex h-11 w-full items-center justify-between rounded-lg border border-zinc-300 px-3.5 py-2.5 shadow-sm 
+      focus:border-violet-300 focus:ring-4 focus:ring-violet-50 data-[placeholder]:text-zinc-500
+      dark:border-zinc-700 dark:bg-zinc-800 dark:focus:border-violet-500 dark:focus:ring-violet-500/10 dark:data-[placeholder]:text-zinc-400
+      `}
+      >
         <SelectPrimitive.Value
           placeholder={placeholder}
           className="text-black"
         />
         <SelectPrimitive.Icon>
-          <ChevronDown className="h-5 w-5 text-zinc-500" />
+          <ChevronDown className="h-5 w-5 text-zinc-500 dark:text-zinc-400" />
         </SelectPrimitive.Icon>
       </SelectPrimitive.Trigger>
 
@@ -26,7 +31,9 @@ export function Select({ children, placeholder, ...rest }: SelectProps) {
           side="bottom"
           position="popper"
           sideOffset={8}
-          className="animate-slideDownAndFade z-10 w-[--radix-select-trigger-width] overflow-hidden rounded-lg border border-zinc-200 bg-white shadow-lg"
+          className={` z-10 w-[--radix-select-trigger-width] animate-slideDownAndFade overflow-hidden rounded-lg border border-zinc-200 bg-white
+          shadow-lg  dark:border-zinc-700 dark:bg-zinc-800
+          `}
         >
           <SelectPrimitive.Viewport>{children}</SelectPrimitive.Viewport>
         </SelectPrimitive.Content>
