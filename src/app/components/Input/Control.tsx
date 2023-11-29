48ec61297feb9561688313bb5489901733b1f834
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-11-29 15:28:29 -0300

    ♻️💄 Refactor input component styles with dark mode 💄 ♻️

--- src/app/components/Input/Control.tsx
+++ src/app/components/Input/Control.tsx
@@ -5,7 +5,10 @@ type ControlProps = ComponentProps<'input'>
 export function Control(props: ControlProps) {
   return (
     <input
-      className="flex flex-1 items-center justify-center border-0 bg-transparent p-0 text-base text-zinc-900 placeholder-zinc-500"
+      className={`flex flex-1 items-center justify-center border-0 bg-transparent p-0 
+      text-base text-zinc-900 placeholder-zinc-500 outline-none
+      dark:text-zinc-100 dark:placeholder-zinc-400
+      `}
       {...props}
     />
   )
--- src/app/components/Input/Root.tsx
+++ src/app/components/Input/Root.tsx
@@ -7,7 +7,10 @@ type RootProps = ComponentProps<'div'> & {
 export function Root({ children, ...rest }: RootProps) {
   return (
     <div
-      className=" flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3.5 py-2.5 text-zinc-500"
+      className={`flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3.5 py-2.5 text-zinc-500 
+      focus-within:border-violet-300 focus-within:ring-4 focus-within:ring-violet-50
+        dark:border-zinc-700 dark:bg-zinc-800 dark:focus-within:border-violet-500 dark:focus-within:ring-violet-500/10
+      `}
       {...rest}
     >
       {children}
