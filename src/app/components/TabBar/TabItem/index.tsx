9a0f7c442ccb5c94d64d2d78e26345fc67c8c33a
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-11-29 15:29:13 -0300

    ♻️💄 Refactor TabBar component to use ScrollArea for horizontal scrolling  and dark mode 💄 ♻️

--- src/app/components/TabBar/TabItem/index.tsx
+++ src/app/components/TabBar/TabItem/index.tsx
@@ -7,25 +7,38 @@ type TabItemProps = {
   isSelected?: boolean
 }
 
-export function TabItem({ title, value, isSelected = false }: TabItemProps) {
+export function TabItem({
+  title,
+  value,
+  isSelected = false,
+}: Readonly<TabItemProps>) {
   return (
     <Tabs.Trigger
       value={value}
       className="
+      group
       relative
-      pb-4
+      whitespace-nowrap
+      pb-4 
       text-sm 
       font-medium 
-      text-zinc-500 
+      text-zinc-500
+      outline-none
       hover:text-violet-700
-      data-[state=active]:text-violet-700"
+      data-[state=active]:text-violet-700
+      dark:text-zinc-400
+      dark:hover:text-violet-300
+      dark:data-[state=active]:text-violet-300
+      "
     >
-      {title}
+      <span className="  group-focus-visible:ring-2 group-focus-visible:ring-violet-400 group-focus-visible:ring-offset-4">
+        {title}
+      </span>
 
       {isSelected && (
         <motion.div
           layoutId="activeTab"
-          className="absolute -bottom-px left-0 right-0  h-0.5 bg-violet-700"
+          className="absolute -bottom-px left-0 right-0  h-0.5 bg-violet-700 dark:bg-violet-300"
         />
       )}
     </Tabs.Trigger>
