c0a9967b998541f3d6a865dda942bc8e70e7f566
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-12-12 12:13:20 -0300

    :fix: Fix sidebar layout overflow issue :fix:

--- src/app/components/SideBar/index.tsx
+++ src/app/components/SideBar/index.tsx
@@ -16,7 +16,7 @@ import { Switch } from './Switch'
 export function SideBar() {
   return (
     <aside>
-      <Collapsible.Root className=" fixed  left-0 right-0 top-0 z-20 flex flex-col gap-6 border-b border-zinc-200 bg-white p-4 data-[state=open]:bottom-0 dark:border-zinc-800 dark:bg-zinc-900 lg:w-80 lg:border-r lg:py-8 lg:data-[state=closed]:bottom-0">
+      <Collapsible.Root className=" fixed left-0  right-0 top-0 z-20 flex flex-col gap-6 overflow-y-auto border-b border-zinc-200 bg-white p-4 data-[state=open]:bottom-0 dark:border-zinc-800 dark:bg-zinc-900 lg:w-80 lg:border-r lg:py-8 lg:data-[state=closed]:bottom-0">
         <header className="mx-1 flex items-center gap-2.5 ">
           <div className="flex w-full items-center justify-between">
             <div className="flex items-center gap-2.5">
